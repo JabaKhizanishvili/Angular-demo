14d0d5ebc1a3978dfc851784128a8adf8039e4a3
JabaKhizanishvili <xizanishvili.99@gmail.com>
2022-02-24 01:42:02 +0400

    inital code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NgbCarouselConfig]
 })
+
+// export class NgbdCarouselConfig {
+//   images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+//   constructor(config: NgbCarouselConfig) {
+//     // customize default values of carousels used by this component tree
+//     config.interval = 10000;
+//     config.wrap = false;
+//     config.keyboard = false;
+//     config.pauseOnHover = false;
+//   }
+// }
+
 export class AppComponent {
-  title = 'jaba';
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
+  color: string = "";
+  title: string = 'Home';
+  link: string = "../assets/vid/test.mp4";
+  click(e: any): void {
+    if (this.color == "red") {
+      this.color = "blue";
+      this.title = "Home";
+    } else {
+      this.color = "red";
+      this.title = "ხოუმი";
+    }
+    e.target.style.backgroundColor = this.color;
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
